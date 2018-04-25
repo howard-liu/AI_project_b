20c18bc3bf948b62c3cd129ee195fc5b2a52d7f4
howa9999 <hwd.liu@gmail.com>
2018-04-25 12:41:49 +1000

    Initialised class

---
+++ Player.py
@@ -0,0 +1,14 @@
+
+
+
+class Player(object):
+
+    def __init__(self, colour):
+        self.colour = colour
+
+
+    def action(self, turns):
+        return
+
+    def update(self, action):
+        return
