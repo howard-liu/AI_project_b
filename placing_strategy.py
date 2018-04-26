b60c825286f3cf6649a0d4b90caf1a0645e5b5e0
howa9999 <hwd.liu@gmail.com>
2018-04-26 11:38:44 +1000

    Some strat planning 2

--- placing_strategy.py
+++ placing_strategy.py
@@ -73,23 +73,37 @@ def first_move(board_state, enemy):
     # If not find a centre piece which does not commit suicide
 
 
-# Enter in a rank (between 1 to 4) and it will give out list of tuples
-def tiles_ranking(rank):
-    tile_ranking_dictionary = {
-        0: 1,
-        1: 2,
-        2: 3,
-        3: 4,
-        4: 4,
-        5: 3,
-        6: 2,
-        7: 1
-    }
-    tuples = []
-    for x in range(7):
-        for y in range(7):
-            if min(tile_ranking_dictionary[x], tile_ranking_dictionary[y]) == rank:
-                tuples.append((x, y))
-    return tuples
+# Enter in a rank (between 0 to 3) and it will give out list of tuples
+def find_tiles_of_rank(rank):
+    """
+    Enter in rank (0-3) (3 is centre) and it will spit out a list of tuples that are in that rank
+    :param rank: Int 0 to 3, where 3 is the centre, 0 is the outermost tiles
+    :return: List of tuples that are in the rank
+    """
+    tile_rank = [([(0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (1, 0), (1, 7),(2, 0), (2, 7), (3, 0), (3, 7),
+                   (4, 0), (4, 7), (5, 0), (5, 7), (6, 0), (6, 7), (7, 1), (7, 2), (7, 3), (7, 4), (7, 5), (7, 6)]),
+                 ([(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (2, 1), (2, 6), (3, 1), (3, 6),
+                   (4, 1), (4, 6), (5, 1), (5, 6), (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6)]),
+                 ([(2, 2), (2, 3), (2, 4), (2, 5), (3, 2), (3, 5), (4, 2), (4, 5), (5, 2), (5, 3), (5, 4), (5, 5)]),
+                 ([(3, 3), (3, 4), (4, 3), (4, 4)])]
+
+    return tile_rank[rank][1]
+    #
+    # tile_ranking_dictionary = {
+    #     0: 1,
+    #     1: 2,
+    #     2: 3,
+    #     3: 4,
+    #     4: 4,
+    #     5: 3,
+    #     6: 2,
+    #     7: 1
+    # }
+    # tuples = []
+    # for x in range(7):
+    #     for y in range(7):
+    #         if min(tile_ranking_dictionary[x], tile_ranking_dictionary[y]) == rank:
+    #             tuples.append((x, y))
+    # return tuples
 
 
